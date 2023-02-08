458c34693f7156802de240e8f5ff565d42895803
unknown <ykh059@naver.com>
2023-02-08 22:31:09 +0900

    first

---
+++ src/main/java/com/example/runi/RuniApplication.java
@@ -0,0 +1,13 @@
+package com.example.runi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuniApplication.class, args);
+	}
+
+}

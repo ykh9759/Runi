7aae5afdf0b1aa711943280e7234c0387d59548d
root <root@ip-172-31-9-227.ap-northeast-2.compute.internal>
2023-02-09 21:40:23 +0900

    20230209 리액트추가

---
+++ src/main/java/com/example/runi/Controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.runi.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public List<String> Hello(){
+        return Arrays.asList("서버서버", "뷰뷰");
+    }
+}

346e2f46390ac0a175d8d784c7da67f118df8e43
unknown <ykh059@naver.com>
2023-03-02 23:18:31 +0900

    20230302 상품내역 수정

--- src/main/resources/static/js/demo/datatables-demo.js
+++ src/main/resources/static/js/demo/datatables-demo.js
@@ -1,7 +1,6 @@
 // Call the dataTables jQuery plugin
 $(document).ready(function() {
-  $('#productTable').DataTable({
-    responsive :true,
+  let productTable = $('#productTable').DataTable({
     dom: '<"m-2"B><"row"<"col-sm-12 col-md-6"l><"col-sm-12 col-md-6"f>>rt<"row"<"col-sm-12 col-md-5"i><"col-sm-12 col-md-7"p>>',
     buttons: [{
       extend: 'excel',
@@ -14,6 +13,7 @@ $(document).ready(function() {
       'type' : 'POST',
       'dataSrc' : ''
     },
+    // destroy: true,
     columns: [
       {data: 'no'},
       {data: 'productName'},
@@ -36,4 +36,46 @@ $(document).ready(function() {
     },
     "order": [[3, 'desc']]
   });
+
+  $('#productTable_filter').prepend('<select id="select"></select>');
+  $('#productTable > thead > tr').children().each(function (indexInArray, valueOfElement) { 
+      $('#select').append('<option>'+valueOfElement.innerHTML+'</option>');
+  });
+
+  $('#select').change(function() {
+    $('.dataTables_filter > label > input').val('');
+  });
+  
+
+  $('.dataTables_filter > label > input').keyup(function () {
+    var colIndex = document.querySelector('#select').selectedIndex;
+    productTable.column(colIndex).search(this.value).draw();
+  });
+
+  $('#productTable_filter').prepend('<input type="text" id="toDate" placeholder="yyyy-MM-dd"> ');
+  $('#productTable_filter').prepend('<input type="text" id="fromDate" placeholder="yyyy-MM-dd">~');
+
+  $.fn.dataTable.ext.search.push(
+    function(settings, data, dataIndex){
+        var min = Date.parse($('#fromDate').val());
+        var max = Date.parse($('#toDate').val());
+        var targetDate = Date.parse(data[3]);
+ 
+        if( (isNaN(min) && isNaN(max) ) || 
+            (isNaN(min) && targetDate <= max )|| 
+            ( min <= targetDate && isNaN(max) ) ||
+            ( targetDate >= min && targetDate <= max) ){ 
+                return true;
+        }
+        return false;
+    }
+);
+
+$('#toDate, #fromDate').keyup( function() {
+  productTable.draw();
+} );
+
+
+
+  
 });
